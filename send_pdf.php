d5abfa8917512549f014d25606227aea81c4fe60
Vanja Mojsilovic <vanja.mojsilovic@gmail.com>
2018-09-18 01:12:58 +0200

    pdf new line

--- send_pdf.php
+++ send_pdf.php
@@ -28,21 +28,15 @@ $file_path="tmp/employees.pdf";
 $pdf = new PDF("L", "mm","A5");
 
 $header = array('ID', 'Name', 'Vocation', 'Address','Phone');
-$pdf->SetFont('Arial','',9);
-$pdf->AddPage();
+
+$pdf->AddPage('L');
+
 $pdf->SetFillColor(255,0,0);
 $pdf->SetTextColor(255);
 $pdf->SetDrawColor(128,0,0);
 $pdf->SetLineWidth(.3);
-$pdf->SetFont('','B','9');
-// Header
-$w_id=15;
-$w_name=40;
-$w_voc=40;
-$w_add=30;
-$w_ph=30;
-
-$w = array($w_id, $w_name, $w_voc, $w_add, $w_ph);
+$pdf->SetFont('Arial','','9');
+$w = array(15, 40, 40, 60, 30);
 for($i=0;$i<count($header);$i++){
     $pdf->Cell($w[$i],7,$header[$i],1,0,'C',true);
 }
@@ -51,41 +45,62 @@ $pdf->Ln();
 $data = $employees;
 $pdf->SetFillColor(224,235,255);
 $pdf->SetTextColor(0);
-$pdf->SetFont('Arial','','11');
+$pdf->SetFont('Arial','','9');
 // Data
 $fill = false;
 for($i=0;$i<count($data);$i++){
     $max_height=6;
     $h_array=array(count($data[$i]['vocation']),count($data[$i]['address']),count($data[$i]['phone']));
     if(max($h_array)>1){
-        $max_height=$max_height*6;
+        $max_height=(max($h_array))*6;
     }
   
-  
-    $pdf->Cell($w[0],$max_height,$data[$i]['id_zaposleni'],'LR',0,'L',$fill);
+    $x_beginning = $pdf->GetX();
+    $pdf->Cell($w[0],$max_height,$data[$i]['id_zaposleni'],1,0,'L',$fill);
     $full_name=$data[$i]['name'].' '.$data[$i]['lastname'];
-    $pdf->Cell($w[1],$max_height,$full_name,'LR',0,'L',$fill);
-    $vocations='';
+    
+    $pdf->Cell($w[1],$max_height,$full_name,1,0,'L',$fill);
+    $vocations=' ';
     if(count($data[$i]['vocation']>0)){
         for($j=0;$j<count($data[$i]['vocation']);$j++){
             $vocations=$vocations.$data[$i]['vocation'][$j]." \n ";
         }
+        $vocations=substr($vocations, 0, -3);
     }
     $x = $pdf->GetX();
     $y = $pdf->GetY();
-    $pdf->MultiCell($w[2],6,$vocations,'LR','L',$fill);
+    $pdf->MultiCell($w[2],6,$vocations,1,'L',$fill);
     $pdf->SetXY($x+$w[2], $y);
-    $pdf->Cell($w[3],$max_height,'','LR',0,'L',$fill);
-    $pdf->Cell($w[4],$max_height,'','LR',0,'L',$fill);
-    $pdf->SetFillColor(224,235,255);
-    $pdf->SetTextColor(0);
-    $pdf->SetFont('');
-        //            $this->Cell($w[2],6,number_format($row[2]),'LR',0,'R',$fill);
-        //            $this->Cell($w[3],6,number_format($row[3]),'LR',0,'R',$fill);
+    
+    $addresses=' ';
+    if(count($data[$i]['address']>0)){
+        for($j=0;$j<count($data[$i]['address']);$j++){
+            $addresses=$addresses.$data[$i]['address'][$j]." \n ";
+        }
+        $addresses=substr($addresses, 0, -3);
+    }
+    $x = $pdf->GetX();
+    $y = $pdf->GetY();
+    $pdf->MultiCell($w[3],6,$addresses,1,'L',$fill);
+    $pdf->SetXY($x+$w[3], $y);
+    
+    $phones=' ';
+    if(count($data[$i]['phone']>0)){
+        for($j=0;$j<count($data[$i]['phone']);$j++){
+            $phones=$phones.$data[$i]['phone'][$j]." \n ";
+        }
+        $phones=substr($phones, 0, -3);
+    }
+   
+    $y = $pdf->GetY();
+    $pdf->MultiCell($w[4],6,$phones,1,'L',$fill);
+
+    
+    
     $pdf->Ln();
     $fill = !$fill;
-    }
-$pdf->Cell(array_sum($w),0,'','T');
+}
+//$pdf->Cell(array_sum($w),0,'','T');
      
        
         
